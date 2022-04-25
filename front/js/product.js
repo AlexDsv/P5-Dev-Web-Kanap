102427afc2c329ec168ca68070593f1a3c1935a1
AlexDsv <Alex.dsv@hotmail.fr>
2022-04-25 13:27:27 +0200

    Produits sur la page d'accueil, démarrage page produit

---
+++ front/js/product.js
@@ -0,0 +1,7 @@
+Si le lien est "xxxxxxxxx/id produit 1"> 
+Alors > Injecter les infos du produit 1 dans le html 
+Sinon > Si le lien "xxxxxxxx/ id produit 2">
+Alors > Injecter les infos du produit 2 dans le html  
+
+
+
