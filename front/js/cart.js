ea163a728d87bf5e53a14a9ccf7a1e0eb699a8ca
AlexDsv <Alex.dsv@hotmail.fr>
2022-05-16 12:50:36 +0200

    Supprimer un produit et modifier quantité ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,32 +4,44 @@ let productInfosObj = JSON.parse(productInfos)
 const selectCart = document.getElementById("cart__items")
 const selectTotalPrice = document.getElementById("totalPrice")
 const selectTotalQuantity = document.getElementById("totalQuantity")
-
+let someProducts = [];
 let storageData = JSON.parse(localStorage.getItem("data"));
-
+if(localStorage.length == 0){
+  emptyCart();
+}
+else{
 for (i = 0; i < storageData.length; i++){
-function getProduct() {
-  return fetch(`http://localhost:3000/api/products/${storageData[i].id}`)
-  .then((res) => {
+  function getProduct() {
+    return fetch(`http://localhost:3000/api/products/${storageData[i].id}`)
+    .then((res) => {
       return res.json();                                      
-  })
-  .then((product) => {
+    })
+    .then((product) => {
       return product;
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
       alert(
-          "Une erreur est survenue."
-          );
+        "Une erreur est survenue."
+        );
       });
+    }
   }
 }
 
 
-
+function emptyCart(){
+  selectCart.innerHTML = '<p>Votre panier est vide</p>'
+  selectTotalPrice.textContent = 0
+  selectTotalQuantity.textContent = 0
+}
 
 
 
 const cartDisplay = async () =>{
+  if(storageData == null){
+    emptyCart();
+  }
+  else{
   for (i = 0; i < storageData.length; i++){
     const product = await getProduct();
     //let multiplPriceQty = `${product.price}`*`${storageData[i].quantity}`
@@ -46,21 +58,86 @@ const cartDisplay = async () =>{
     <div class="cart__item__content__settings">
       <div class="cart__item__content__settings__quantity">
         <p>Qté : ${storageData[i].quantity}</p>
-        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${storageData[i].quantity}">
+        <input type="number" class="itemQuantity" name="itemQuantity" data-id="${storageData[i].id}" data-color="${storageData[i]}}" min="1" max="100" value="${storageData[i].quantity}">
       </div>
       <div class="cart__item__content__settings__delete">
-        <p class="deleteItem" id="deleteProduct${[i]}">Supprimer</p>
+        <p class="deleteItem" data-id="${storageData[i].id}" data-color=${storageData[i].color}>Supprimer</p>
       </div>
     </div>
   </div>
   </article>`
-  }
+}
+}
+changeQuantity();
+removeProduct();
+totalCartPrice();
+totalCartQuantity();
 }  
 
 cartDisplay();
 
 
 
+const removeProduct = async (cartDisplay) =>{
+  await cartDisplay;
+  let deleteBtns = document.querySelectorAll(".deleteItem");
+  let productToRemove = storageData.length
+  
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', () => {
+      
+      if(productToRemove == 1){
+        return (
+          localStorage.removeItem('data'),
+          location.reload()
+        )
+      }
+
+      else {
+        someProducts = storageData.filter((el) =>{
+          if (deleteBtn.dataset.id != el.id || deleteBtn.dataset.color != el.color){
+            console.log(deleteBtn.dataset.id);
+            return true;
+          }
+        })
+       console.log(someProducts);
+       localStorage.setItem('data', JSON.stringify(someProducts));
+       location.reload()
+      }
+});
+  
+});
+return;
+};
+
+const changeQuantity = async (cartDisplay) =>{
+await cartDisplay;
+console.log('ok');
+const selectQuantityBtns = document.querySelectorAll(".itemQuantity");
+
+console.log(selectQuantityBtns);
+
+selectQuantityBtns.forEach((quantityBtn) => {
+  console.log(quantityBtn);
+  
+  quantityBtn.addEventListener('change', () => {
+    console.log('ok2');
+    console.log(quantityBtn.value);
+    console.log(quantityBtn.dataset.id);
+    for(i=0; i < storageData.length; i++){
+      
+        console.log('ok3');
+        return storageData[i].quantity=quantityBtn.value,
+        console.log(storageData[i].quantity),
+        localStorage.setItem("data", JSON.stringify(storageData)),
+        (document.querySelectorAll('.cart__item__content__settings__quantity > p')[i].textContent = storageData[i].quantity)
+
+    }
+  }
+)})
+}
 
 
-
+const totalCartPrice = async (cartDisplay) =>{
+  await cartDisplay
+}
